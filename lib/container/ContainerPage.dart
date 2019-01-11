78100770bd9c93aa9318e769b3a25bb82d66d518
zhh <zhenghong1991@126.com>
2019-01-11 17:30:19 +0800

    Update ContainerPage.dart

--- lib/container/ContainerPage.dart
+++ lib/container/ContainerPage.dart
@@ -8,24 +8,25 @@ class ContainerPage extends StatefulWidget {
 class _ContainerPageState extends State<ContainerPage> {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    double pixelRatio = MediaQuery.of(context).devicePixelRatio;
     return Scaffold(
         appBar: new AppBar(
           title: Text("ContainerPage"),
         ),
         body: new Container(
             //四周大小为10的margin
-            margin: EdgeInsets.all(10),
             height: 120,
-            width: 500,
+            width: 360,
             //背景装饰
             decoration: new BoxDecoration(
                 //圆角
-                borderRadius: BorderRadius.all(Radius.circular(4.0)),
                 //背景色
                 color: Colors.blue,
                 //边框
                 border: new Border.all(color: Colors.white, width: 0.5)),
             child:
-                new Text("666666", style: new TextStyle(color: Colors.white))));
+                new Text("width:$width height:$height pixelRatio:$pixelRatio", style: new TextStyle(color: Colors.white))));
   }
 }
